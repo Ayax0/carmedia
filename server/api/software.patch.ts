9cf765bf6b4a13f497e3d6742d1fc53f7e96e42f
Ayax0 <simonskyskate@hotmail.com>
2022-11-10 20:25:15 +0100

    change production path

--- server/api/software.patch.ts
+++ server/api/software.patch.ts
@@ -1,10 +1,13 @@
 import { exec } from "child_process";
+import path from "path";
 
 export default defineEventHandler(async (event) => {
     // if(process.env.NODE_ENV != "production") return;
+    const root = process.env.NODE_ENV == "production" ? path.join(process.cwd(), "../", "../") : path.join(process.cwd());
+
     try {
         await new Promise((resolve, reject) => {
-            exec("cd " + process.cwd() + "&& npm i && npm i --save-dev && npm run build", (error, stdout) => {
+            exec("cd " + root + "&& npm i && npm i --save-dev && npm run build", (error, stdout) => {
                 if(error) reject(error);
                 else resolve(stdout);
             });
