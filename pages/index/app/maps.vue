682734a7456e0d07f8905701bb1cbe7f5be2728f
Simon Gander <simon_gander@nextlvlup.com>
2022-12-02 17:45:03 +0100

    3d graphics implementation

--- pages/index/app/maps.vue
+++ pages/index/app/maps.vue
@@ -1,28 +1,65 @@
-<script>
-import { vue3dLoader } from "vue-3d-loader";
+<script lang="ts">
+import * as THREE from "three";
+import FBXLoader from "@/utils/fbxloader";
 
 export default {
-    components: {
-        vue3dLoader
-    },
     methods: {
         degToRad(deg) {
             return deg * Math.PI / 180
         }
     },
+    mounted() {
+        const camera = new THREE.PerspectiveCamera();
+
+        camera.position.z = 800;
+
+        const scene = new THREE.Scene();
+
+        const light = new THREE.DirectionalLight(0xfffffb, 0.5); // soft white light
+        light.position.z = 800;
+        scene.add( light );
+
+        const loader = new FBXLoader();
+        
+        loader.load("/3d/opelcorsac.fbx", (fbx) => {
+            console.log("Loaded:", fbx);
+            
+            fbx.children = fbx.children.filter((children) => children instanceof THREE.Mesh);
+            fbx.children.forEach((mesh: THREE.Mesh) => {
+                mesh.material.find((material: THREE.MeshPhongMaterial) => material.name == "MainColor").color = new THREE.Color(0xff0000);
+                
+                const lights = mesh.material.find((material: THREE.MeshPhongMaterial) => material.name == "Lights");
+                const backlights = mesh.material.find((material: THREE.MeshPhongMaterial) => material.name == "BackLight");
+
+                lights.emissive = new THREE.Color(0xfffffb);
+                lights.emissiveIntensity = 50;
+
+                backlights.emissive = new THREE.Color(0xff0000);
+                backlights.emissiveIntensity = 200;
+            })
+
+            fbx.rotation.x = this.degToRad(60);
+            fbx.rotation.y = this.degToRad(180);
+
+            scene.add(fbx);
+
+            const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+            renderer.setSize(300, 300);
+            /*renderer.setAnimationLoop((time) => {
+                fbx.rotation.y = time / 2000;
+
+                renderer.render(scene, camera);
+            });*/
+
+            renderer.render(scene, camera);
+
+            document.getElementById("render_screen").appendChild(renderer.domElement);
+        }, () => console.log("progress"),
+        (error) => console.log(error));
+    }
 }
 </script>
 
 <template>
-    <div>
-        <ClientOnly>
-            <vue3dLoader
-                :height="347"
-                :backgroundAlpha="0"
-                :cameraPosition="{ x: 0, y: 0, z: 2500 }"
-                :rotation="{ x: degToRad(30), y: degToRad(180), z: 0 }"
-                filePath="/assets/3d/opelcorsac.fbx"
-            />
-        </ClientOnly>
-    </div>
+    <div id="render_screen" style="margin: calc(50vh - 150px - 4rem) calc(50vw - 150px)"></div>
 </template>
