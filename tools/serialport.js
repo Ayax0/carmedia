27e74bce4386a3cc6d4e9cb7393aeef8da755bb7
Ayax0 <simonskyskate@hotmail.com>
2023-01-30 18:23:50 +0100

    fix fs bug

--- tools/serialport.js
+++ tools/serialport.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 
 if (!fs.existsSync("../.output/server/node_modules/@serialport/bindings-cpp/prebuilds")) {
     console.log("serialport: no prebuilds found");
-    fs.cpSync("../node_modules/@serialport/bindings-cpp/prebuilds", "../.output/server/node_modules/@serialport/bindings-cpp/prebuilds");
+    fs.copyFileSync("../node_modules/@serialport/bindings-cpp/prebuilds", "../.output/server/node_modules/@serialport/bindings-cpp/prebuilds");
     console.log("serialport: prebuilds copied successfuly");
 }
