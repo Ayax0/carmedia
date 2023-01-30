cff5c52b3bb9519c119f891d33be6e7f61440df5
Ayax0 <simonskyskate@hotmail.com>
2023-01-30 18:29:23 +0100

    switch to fs-extra

--- tools/serialport.js
+++ tools/serialport.js
@@ -1,7 +1,8 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 
-if (!fs.existsSync("../.output/server/node_modules/@serialport/bindings-cpp/prebuilds")) {
+if (!fs.pathExistsSync("../.output/server/node_modules/@serialport/bindings-cpp/prebuilds")) {
     console.log("serialport: no prebuilds found");
-    fs.copyFileSync("../node_modules/@serialport/bindings-cpp/prebuilds", "../.output/server/node_modules/@serialport/bindings-cpp/prebuilds");
+    fs.ensureDirSync("../.output/server/node_modules/@serialport/bindings-cpp/prebuilds");
+    fs.copySync("../node_modules/@serialport/bindings-cpp/prebuilds", "../.output/server/node_modules/@serialport/bindings-cpp/prebuilds");
     console.log("serialport: prebuilds copied successfuly");
 }
