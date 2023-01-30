1e6cc91c1b8473a5248972e8c24d3beb519ebcda
Ayax0 <simonskyskate@hotmail.com>
2023-01-30 18:49:41 +0100

    fix path

--- tools/serialport.js
+++ tools/serialport.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 const path = require("path");
 const home = path.join(__dirname, "..");
 
-if (!fs.pathExistsSync(path.join(home, "../.output/server/node_modules/@serialport/bindings-cpp/prebuilds"))) {
+if (!fs.pathExistsSync(path.join(home, ".output/server/node_modules/@serialport/bindings-cpp/prebuilds"))) {
     console.log("serialport: no prebuilds found");
     fs.ensureDirSync(path.join(home, ".output/server/node_modules/@serialport/bindings-cpp/prebuilds"));
     fs.copySync(
